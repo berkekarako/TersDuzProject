0c1aa2107c81b55b186b671a3915b1275661aaeb
berkekarako <berkekarakoccc@gmail.com>
2023-06-18 02:07:12 +0300

    PlayerMovement Scripti değişti

--- TersDuz/Assets/Scripts/PlayerMovement.cs
+++ TersDuz/Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,36 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float horizontal = 0f;
+    [SerializeField] private float moveSpeed = 8f;
+    [SerializeField] private float jumpingPower = 16f;
+    private bool isFacingRight = true;
+
     private Rigidbody2D rb;
     private BoxCollider2D coll;
     private SpriteRenderer sprite;
     private Animator anim;
 
+    private bool isWallSliding;
+    private float wallSlidingSpeed = 2f;
+    
+    private bool isWallJumping;
+    private float wallJumpingDirection;
+    private float wallJumpingTime = 0.2f;
+    private float wallJumpingCounter;
+    private float wallJumpingDuration;
+    private Vector2 wallJumpingPower = new Vector2(8f, 16f);
+
     [SerializeField] private LayerMask jumpableGround;
 
-    private float dirX = 0f;
-    [SerializeField] private float moveSpeed = 8f;
-    [SerializeField] private float jumpForce = 13f;
+    
 
-    private int jump = 0;
+    [SerializeField] private Transform wallCheck;
+    [SerializeField] private LayerMask wallLayer;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask groundLayer;
+
+    //private int jump = 0;
 
     private enum MovementState { idle, running, jumping, falling }
 
@@ -32,42 +50,54 @@ public class PlayerMovement : MonoBehaviour
 
     private void Update()
     {
-        dirX = Input.GetAxisRaw("Horizontal");
+        horizontal = Input.GetAxisRaw("Horizontal");
 
-        if(rb.bodyType != RigidbodyType2D.Static)
-        {
-            rb.velocity = new Vector2(dirX * moveSpeed, rb.velocity.y);
-        }
-        
-        if(IsGrounded())
-        {
-            jump = 0;
-        }
+        //if(rb.bodyType != RigidbodyType2D.Static && !isWallJumping)
+        //{
+        //    rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
+        //}
 
-        if(Input.GetButtonDown("Jump"))
+        if(Input.GetButtonDown("Jump") && IsGrounded())
         {
-            if (jump == 0)
-            {
-                jumpSoundEffect.Play();
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                jump++;
-            }
-            else if (jump == 1 && IsGrounded())
-            {
-                jumpSoundEffect.Play();
-                rb.velocity = new Vector2(rb.velocity.x, jumpForce);
-                jump++;
-            }
+            rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            jumpableGround++;
         }
 
-       UpdateAnimationState();
+        if (Input.GetButtonDown("Jump") && rb.velocity.y >0f)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+        {
+            //if (jump == 0)
+            //{
+            //    jumpSoundEffect.Play();
+            //    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            //    jump++;
+            //}
+            //else if (jump == 1 && IsGrounded())
+            //{
+            //    jumpSoundEffect.Play();
+            //    rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
+            //    jump++;
+            //}
+        }
+
+        UpdateAnimationState();
+        Flip();
+        WallSide();
+        WallJump();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(horizontal * moveSpeed, rb.velocity.y);
     }
 
     private void UpdateAnimationState()
     {
         MovementState state;
 
-        if (dirX > 0f)
+        if (horizontal > 0f)
         {
             if (rb.bodyType != RigidbodyType2D.Static)
             {
@@ -75,7 +105,7 @@ public class PlayerMovement : MonoBehaviour
             }
             state = MovementState.running;
         }
-        else if (dirX < 0)
+        else if (horizontal < 0)
         {
             if (rb.bodyType != RigidbodyType2D.Static)
             {
@@ -102,6 +132,74 @@ public class PlayerMovement : MonoBehaviour
 
     private bool IsGrounded()
     {
-       return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+        return Physics2D.OverlapCircle(groundCheck.position, 0.2f, groundLayer);
+        //return Physics2D.BoxCast(coll.bounds.center, coll.bounds.size, 0f, Vector2.down, .1f, jumpableGround);
+    }
+
+    private bool IsWalled()
+    {
+        return Physics2D.OverlapCircle(wallCheck.position, 0.2f, wallLayer);
+    }
+
+    private void WallSide()
+    {
+        if (IsWalled() && !IsGrounded() && horizontal != 0f)
+        {
+            isWallSliding = true;
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Clamp(rb.velocity.y, -wallSlidingSpeed, float.MaxValue));
+        }
+        else 
+        { 
+        isWallSliding = false;
+        }
+    }
+
+    private void WallJump()
+    {
+        if(isWallSliding)
+        {
+            isWallJumping = false;
+            wallJumpingDirection = -transform.localScale.x;
+            wallJumpingCounter = wallJumpingTime;
+
+            CancelInvoke(nameof(StopWallJumping));
+        }
+        else
+        {
+            wallJumpingCounter = -Time.deltaTime;
+        }
+
+        if(Input.GetButtonDown("Jump") && wallJumpingCounter > 0f)
+        {
+            isWallJumping = true;
+            rb.velocity = new Vector2(wallJumpingDirection * wallJumpingPower.x, wallJumpingPower.y);
+            wallJumpingCounter = 0f;
+
+            if(transform.localScale.x != wallJumpingDirection)
+            {
+                isFacingRight = !isFacingRight;
+                Vector3 localScale = transform.localScale;
+                localScale.x *= -1f;
+                transform.localScale = localScale;
+            }
+
+            Invoke(nameof(StopWallJumping), wallJumpingDuration);
+        }
+    }
+
+    private void StopWallJumping()
+    {
+        isWallJumping = false;
+    }
+
+    private void Flip()
+    {
+        if(isFacingRight && horizontal <0f || !isFacingRight && horizontal > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 localScale = transform.localScale;
+            localScale.x *= -1f;
+            transform.localScale = localScale;
+        }
     }
 }
