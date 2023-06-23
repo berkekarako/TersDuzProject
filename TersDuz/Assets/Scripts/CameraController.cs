bd76beda8c215a683a200169acad23c1710b96e0
berkekarako <berkekarakoccc@gmail.com>
2023-06-24 01:43:10 +0300

    Level 1

--- TersDuz/Assets/Scripts/CameraController.cs
+++ TersDuz/Assets/Scripts/CameraController.cs
@@ -7,6 +7,6 @@ public class CameraController : MonoBehaviour
     [SerializeField] private Transform player;
     private void Update()
     {
-        transform.position = new Vector3(player.position.x, player.position.y, transform.position.z);
+        transform.position = new Vector3(player.position.x, player.position.y+2, transform.position.z);
     }
 }
